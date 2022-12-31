7057b81e191f5f4057473791356a6f07b57b7c75
minojiro <minojiro@users.noreply.github.com>
2022-12-31 23:57:29 +0900

    feat: First release

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+const primeNumbers = computed(() => {
+  return [
+    2, 3, 5, 7, 11
+  ]
+})
+</script>
+
+<template>
+  <main>
+    <h1>素数</h1>
+    <ul>
+      <li v-for="number in primeNumbers">{{ number }}</li>
+    </ul>
+  </main>
+</template>
